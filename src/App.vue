f831e1158b930d1247a90741417558fa1d429cf0
XuedaoYuan <xdyuan314@gmail.com>
2018-09-28 14:03:18 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <div id="nav">
+            <router-link class="nav-tab" to="/">产品分类</router-link>
+            <router-link class="nav-tab" to="/about">About</router-link>
+        </div>
+        <div id="container">
+          <router-view/>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+#app {
+    // padding: 0 40px;
+}
+
+#nav {
+    height: 80px;
+    background-color: black;
+    line-height: 80px;
+    padding: 0 40px;
+    .nav-tab {
+        font-size: 20px;
+        color: #ccc;
+        margin-right: 20px;
+    }
+
+}
+#container {
+  padding: 0 40px;
+}
+</style>
---
+++ src/views/Category.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+	<ul>
+		<li v-for="item in list">
+			<span>{{item.id}}</span>-<span>{{item.name}}</span>
+		</li>
+	</ul>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  data(){
+  	return {
+  		list: [],
+  	}
+  },
+  components: {
+
+  },
+  created(){
+  	this.$axios({
+  		url: "/tmall/categories",
+  		method: "GET"
+  	}).then(res => {
+  		console.log(res);
+  		this.list = res.list;
+  	})
+  }
+}
+</script>
